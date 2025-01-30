3c327cc346940d7af7d83b44e81b9a1985640d85
Gamart <fabien.gamart@groupe-esigelec.org>
2025-01-30 15:59:39 +0100

    Fixing docker files/scripts and requirements.txt to be compatible

--- Backend/app.py
+++ Backend/app.py
@@ -182,4 +182,4 @@ if __name__ == '__main__':
     print(f"- Processed: {PROCESSED_FOLDER}")
     print(f"- Models: {MODELS_FOLDER}")
     print("Démarrage du serveur Flask sur http://localhost:5001")
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=5001)
